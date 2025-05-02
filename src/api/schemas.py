c9703e14eb9e2f07663d307db46e7c7a788fb027
NathanF <nfant01@gmail.com>
2025-05-02 13:13:26 -0500

    weatherforecastout type

---
+++ src/api/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from datetime import date, time
+
+
+class WeatherForecastOut(BaseModel):
+    forecast_date: date
+    min_temp: float
+    max_temp: float
+    precipitation_chance: float
+    precipitation_amount: float
+    sunrise: time
+    sunset: time
--- src/api/weather.py
+++ src/api/weather.py
@@ -7,20 +7,21 @@ load_dotenv(dotenv_path="src/api/.env")
 
 # Fetch the weather API key from the environment
 WEATHER_API = os.getenv("WEATHER_API")
+api_portion = f"&appid={WEATHER_API}"
 
 # Ensure WEATHER_API is loaded
 if not WEATHER_API:
     raise ValueError("No WEATHER_API key found in .env file")
 
-BASE_URL = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}"
+BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
 
 
-def get_weather_by_coords(longitude, latitude):
-    params = {"lon": longitude, "lat": latitude, "appid": WEATHER_API}
-
-    res = requests.get(BASE_URL, params=params)
-    res.raise_for_status()  # Will raise an error if the response status code is not 200
-    return res.json()
+# https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}
+def get_weather_by_coords(longitude: int, latitude: int) -> str:
+    lon = str(longitude)
+    lat = str(latitude)
+    new_url = BASE_URL + f"?lat={lat}&lon={lon}{api_portion}"
+    return new_url
 
 
 # Test the function
