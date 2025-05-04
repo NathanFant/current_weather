25e9da46dd1be6b1fca903ff7908bbc77059b00e
NathanF <nfant01@gmail.com>
2025-05-04 18:54:49 -0500

    maybe backend complete

--- src/api/api.py
+++ src/api/api.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from .weather import get_temp_by_coords
-from .schemas import Coordinates
+from .schemas import Coordinates, WeatherData
 
 router = APIRouter()
 
 
-@router.post("/weather")
+@router.post("/weather", response_model=WeatherData)
 def get_weather(coords: Coordinates):
     return {"temp_f": get_temp_by_coords(coords.longitude, coords.latitude)}
--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api import router as weather_router
 
 app = FastAPI()
 app.include_router(weather_router, prefix="/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000"
+    ],  # TODO: add website 'frontend.com' when prod launch.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
