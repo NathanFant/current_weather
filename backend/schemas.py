0ee91d0c7e60c4d10692296250ddf5ff0e878027
NathanF <nfant01@gmail.com>
2025-05-13 14:11:03 -0500

    shooting in the dark

--- backend/schemas.py
+++ backend/schemas.py
@@ -24,3 +24,20 @@ class WeatherData(BaseModel):
     temperature: Temperature
     humidity: int
     weather: WeatherDescription
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        orm_mode = True
+
+
+class LoginRequest(UserCreate):
+    pass
